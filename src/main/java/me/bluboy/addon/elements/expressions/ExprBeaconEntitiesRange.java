6d7f623c724ff699b31fb21a3196e919aa9ec594
youngmasterseth <setheweintraub@me.com>
2020-11-02 15:34:18 -0800

    v1.0 : adding CondHasStung, CondIsShivering, ExprBeaconEntitiesRange

---
+++ src/main/java/me/bluboy/addon/elements/expressions/ExprBeaconEntitiesRange.java
@@ -0,0 +1,60 @@
+package me.bluboy.addon.elements.expressions;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.expressions.base.SimplePropertyExpression;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.ExpressionType;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.skript.lang.util.SimpleExpression;
+import ch.njol.util.Kleenean;
+import org.bukkit.block.Beacon;
+import org.bukkit.block.Block;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.event.Event;
+import org.jetbrains.annotations.Nullable;
+
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class ExprBeaconEntitiesRange extends SimpleExpression<LivingEntity> {
+
+    static {
+        Skript.registerExpression(ExprBeaconEntitiesRange.class, LivingEntity.class, ExpressionType.COMBINED, "entit(ys|ies) in range of %blocks%", "%blocks%'[s] entit(ys|ies) in range");
+    }
+
+    private Expression<Block> block;
+
+    @Nullable
+    @Override
+    protected LivingEntity[] get(Event event) {
+        Block b = block.getSingle(event);
+        if (b == null || !(b.getState() instanceof Beacon)) return null;
+        List<LivingEntity> ents = new ArrayList<>();
+        for (LivingEntity ent : ((Beacon)b).getEntitiesInRange()) {
+            ents.add(ent);
+        }
+        LivingEntity[] array = ents.toArray(new LivingEntity[0]);
+        return array;
+    }
+
+    @Override
+    public boolean isSingle() {
+        return false;
+    }
+
+    @Override
+    public Class<? extends LivingEntity> getReturnType() {
+        return LivingEntity.class;
+    }
+
+    @Override
+    public String toString(@Nullable Event event, boolean b) {
+        return "entities in range" + (block != null ? " of "+block.toString(event, b) : "");
+    }
+
+    @Override
+    public boolean init(Expression<?>[] expressions, int i, Kleenean kleenean, SkriptParser.ParseResult parseResult) {
+        this.block = (Expression<Block>)expressions[0];
+        return true;
+    }
+}
---
+++ src/main/java/me/bluboy/addon/elements/expressions/ExprBeeStung.java
@@ -0,0 +1,56 @@
+package me.bluboy.addon.elements.expressions;
+
+import ch.njol.skript.classes.Changer.ChangeMode;
+import ch.njol.skript.expressions.base.SimplePropertyExpression;
+import org.bukkit.entity.Bee;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.TropicalFish;
+import org.bukkit.event.Event;
+import org.jetbrains.annotations.Nullable;
+
+public class ExprBeeStung extends SimplePropertyExpression<LivingEntity, Boolean> {
+
+    static {
+        register(ExprBeeStung.class, Boolean.class, "stung", "livingentities");
+    }
+
+    @Override
+    @Nullable
+    public Boolean convert(final LivingEntity e) {
+        if (!(e instanceof Bee)) return null;
+        return ((Bee)e).hasStung();
+    }
+
+    @Override
+    @Nullable
+    public Class<?>[] acceptChange(final ChangeMode mode) {
+        if (mode == ChangeMode.SET || mode == ChangeMode.DELETE || mode == ChangeMode.RESET)
+            return new Class[] {Boolean.class};
+        return null;
+    }
+
+    @Override
+    public void change(final Event e, @Nullable final Object[] delta, final ChangeMode mode) {
+        if (delta == null) {
+            for (final LivingEntity entity : getExpr().getArray(e)) {
+                ((Bee)entity).setHasStung(false);
+            }
+        } else {
+            final Boolean bool = (Boolean) delta[0];
+            for (final LivingEntity entity : getExpr().getArray(e)) {
+                ((Bee)entity).setHasStung(bool);
+            }
+        }
+    }
+
+    @Override
+    protected String getPropertyName() {
+        return "stung";
+    }
+
+    @Override
+    public Class<? extends Boolean> getReturnType() {
+        return Boolean.class;
+    }
+
+}
